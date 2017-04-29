63d7ff8151533013a625f8c00deb30d8a9136c5d
Unknown <john.boltjes@outlook.com>
2017-04-29 14:06:24 -0500

    Add Mod Files
    Adding in the mod files

---
+++ common/defines/shattered_europa.lua
@@ -0,0 +1,7 @@
+NDefines.NGame.START_DATE = "1444.11.1"
+NDefines.NGame.END_DATE = "3017.01.01"
+NDefines.NNationDesigner.POINTS_AVAILABLE = 500000
+NDefines.NNationDesigner.RULER_MIN_AGE = 1
+NDefines.NNationDesigner.RULER_MAX_AGE = 60
+NDefines.NNationDesigner.HEIR_MAX_AGE = 40
+NDefines.NCountry.ALLOW_FEMALE_GENERALS = 1
