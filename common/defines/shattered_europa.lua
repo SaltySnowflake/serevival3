04db42ea785f88d80cb3801f3a94b3c7b5a01f3a
Simen Fjeld-Olsen <simen.fjeldolsen@gmail.com>
2017-07-15 16:56:57 +0200

    Removed Banner Age multiplier
    Banners are plenty strong as is, especially with culture conversion and developig being easier in Shattered.

--- common/defines/shattered_europa.lua
+++ common/defines/shattered_europa.lua
@@ -1,8 +1,13 @@
 NDefines.NGame.START_DATE = "1444.11.1"
 NDefines.NGame.END_DATE = "3017.01.01"
+
 NDefines.NNationDesigner.POINTS_AVAILABLE = 500000
 NDefines.NNationDesigner.RULER_MIN_AGE = 1
 NDefines.NNationDesigner.RULER_MAX_AGE = 60
 NDefines.NNationDesigner.HEIR_MAX_AGE = 40
+
 NDefines.NCountry.ALLOW_FEMALE_GENERALS = 1
+
 NDefines.NEconomy.MINIMUM_INTERESTS = 1
+
+NDefines.NMilitary.BANNER_AGE_MULTIPLIER = 0.00
